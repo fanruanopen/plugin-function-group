ebe590c56528aed37d2c57629398e5a6d34c36fe
richie <richie@finereport.com>
2016-11-21 19:14:03 +0800

    GetIP函数优化

--- src/com/fr/solution/plugin/function/GetIP.java
+++ src/com/fr/solution/plugin/function/GetIP.java
@@ -1,9 +1,11 @@
 package com.fr.solution.plugin.function;
 
+import com.fr.base.Formula;
 import com.fr.general.GeneralUtils;
 import com.fr.script.AbstractFunction;
 import com.fr.stable.ArrayUtils;
 import com.fr.stable.Primitive;
+import com.fr.stable.UtilEvalError;
 import com.fr.web.core.SessionDealWith;
 import com.fr.web.core.SessionIDInfor;
 
@@ -11,10 +13,22 @@ public class GetIP extends AbstractSolutionFunction {
 
     @Override
     public Object solve(Object[] args) {
+        String sessionID = null;
         if (ArrayUtils.isEmpty(args)) {
-            return Primitive.ERROR_VALUE;
+            try {
+                Object obj = getCalculator().eval(new Formula("=$sessionID"));
+                sessionID = GeneralUtils.objectToString(obj);
+            } catch (UtilEvalError utilEvalError) {
+                utilEvalError.printStackTrace();
+            }
+        } else {
+            sessionID = GeneralUtils.objectToString(args[0]);
         }
-        SessionIDInfor infor = SessionDealWith.getSessionIDInfor(GeneralUtils.objectToString(args[0]));
+
+        SessionIDInfor infor = SessionDealWith.getSessionIDInfor(sessionID);
+        if (infor == null) {
+            return Primitive.NULL;
+        }
         return infor.getRemoteAddress();
     }
 }
